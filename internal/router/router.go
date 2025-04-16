d1c1a0d41b5ec942e9d653511fb242ce708a8646
szyuban <zubans@mail.ru>
2025-04-16 13:30:19 +0300

    iter7

--- internal/router/router.go
+++ internal/router/router.go
@@ -18,7 +18,9 @@ func GetRouter(h *handler.Handler) http.Handler {
 		})
 	})
 	r.Post("/update/", h.UpdateMetricJSON)
+	r.Post("/update", h.UpdateMetricJSON)
 	r.Post("/value/", h.GetMetricJSON)
+	r.Post("/value", h.GetMetricJSON)
 
 	return r
 }
