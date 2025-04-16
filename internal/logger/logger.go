68a3aa688d6b3a0a4c2015b22d825032848a2466
szyuban <zubans@mail.ru>
2025-04-16 11:54:48 +0300

    iter7

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,7 +1,9 @@
 package logger
 
 import (
+	"bytes"
 	"go.uber.org/zap"
+	"io"
 	"net/http"
 	"time"
 )
@@ -23,6 +25,7 @@ type (
 type RequestInfo struct {
 	Method string `json:"method"`
 	URL    string `json:"url"`
+	BODY   string
 	Time   string `json:"time"`
 }
 
@@ -67,6 +70,18 @@ func RequestLogger(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 
+		var bodyString string
+		if r.Method == http.MethodPost || r.Method == http.MethodPut {
+			bodyBytes, err := io.ReadAll(r.Body)
+			if err != nil {
+				Log.Error("failed to read request body", zap.Error(err))
+			} else {
+				bodyString = string(bodyBytes)
+				// Восстанавливаем тело запроса
+				r.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
+			}
+		}
+
 		responseData := &responseData{
 			status: 0,
 			size:   0,
@@ -83,6 +98,7 @@ func RequestLogger(h http.Handler) http.Handler {
 			zap.Any("request", RequestInfo{
 				Method: r.Method,
 				URL:    r.URL.String(),
+				BODY:   bodyString,
 				Time:   duration.String(),
 			}),
 			zap.Any("response", ResponseInfo{
