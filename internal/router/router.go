6fa6537b2cf078e182f8e5ecca90743b4a64767d
szyuban <zubans@mail.ru>
2025-04-20 20:56:56 +0300

    iter8

--- internal/router/router.go
+++ internal/router/router.go
@@ -26,6 +26,15 @@ func GetRouter(h *handler.Handler) http.Handler {
 	return r
 }
 
+type gzipResponseWriter struct {
+	http.ResponseWriter
+	Writer *gzip.Writer
+}
+
+func (w gzipResponseWriter) Write(b []byte) (int, error) {
+	return w.Writer.Write(b)
+}
+
 func GzipMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if strings.Contains(r.Header.Get("Content-Encoding"), "gzip") {
@@ -42,6 +51,19 @@ func GzipMiddleware(next http.Handler) http.Handler {
 				}
 			}(gzReader)
 			r.Body = gzReader
+
+			w.Header().Set("Content-Encoding", "gzip")
+			gw := gzip.NewWriter(w)
+			defer func(gw *gzip.Writer) {
+				err := gw.Close()
+				if err != nil {
+					http.Error(w, "Error close gzWriter", http.StatusBadRequest)
+				}
+			}(gw)
+
+			gzipWriter := gzipResponseWriter{Writer: gw, ResponseWriter: w}
+			next.ServeHTTP(gzipWriter, r)
+			return
 		}
 
 		next.ServeHTTP(w, r)
