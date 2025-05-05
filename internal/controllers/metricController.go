14a0213c6992f2d49834eaa1e66c0113964b2868
szyuban <zubans@mail.ru>
2025-05-05 21:21:38 +0300

    iter12

--- internal/controllers/metricController.go
+++ internal/controllers/metricController.go
@@ -91,3 +91,56 @@ func (mc *MetricsController) JSONSendMetrics() {
 		log.Printf("Failed to send metric: %v, status code: %d\n", metrics, response.StatusCode)
 	}
 }
+
+func (mc *MetricsController) OldJSONSendMetrics() {
+	metrics := mc.metricsService.GetMetrics()
+	dtoMetrics := models.ConvertMetricsListToDTO(metrics.MetricList)
+
+	url := fmt.Sprintf("http://%s/update/", mc.metricsService.Cfg.AddressServer)
+
+	for _, metric := range dtoMetrics {
+		b, err := json.Marshal(metric)
+		if err != nil {
+			log.Printf("Error json encode metric data")
+		}
+
+		var buf bytes.Buffer
+
+		gz := gzipNewWriter(&buf)
+
+		_, err = gz.Write(b)
+		if err != nil {
+			log.Println("Error compressing metric data")
+			return
+		}
+
+		err = gz.Close()
+		if err != nil {
+			log.Println("Error close gzip compressor")
+			return
+		}
+
+		req, _ := http.NewRequest("POST", url, &buf)
+
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Content-Encoding", "gzip")
+
+		response, err := mc.httpClient.Do(req)
+		if err != nil {
+			log.Printf("Error sending metric %s: %v. BODY: %v\n", metric.ID, err, metric)
+			continue
+		}
+
+		err = response.Body.Close()
+		if err != nil {
+			log.Printf("Failed to close Body: %s\n", err)
+			return
+		}
+
+		if response.StatusCode == http.StatusOK {
+			log.Printf("Successfully sent metric: %s\n", metric.ID)
+		} else {
+			log.Printf("Failed to send metric: %s, status code: %d\n", metric.ID, response.StatusCode)
+		}
+	}
+}
