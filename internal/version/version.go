439f287efaf15123b1d8990ee9c285156eba46ca
szyuban <zubans@mail.ru>
2025-07-27 15:45:15 +0300

    add go gen version

--- internal/version/version.go
+++ internal/version/version.go
@@ -7,6 +7,12 @@ package version
 
 import "fmt"
 
+var (
+	BuildVersion = "N/A"
+	BuildDate    = "2025-07-27_12:43:44_UTC"
+	BuildCommit  = "3251c77666a7c95c22856b795f5ce00451204525"
+)
+
 func PrintBuildInfo() {
 	fmt.Printf("Build version: %s\n", BuildVersion)
 	fmt.Printf("Build date: %s\n", BuildDate)
