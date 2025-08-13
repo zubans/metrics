2f033f0c887d1ec6b0804659cc17cbbaad225511
szyuban <zubans@mail.ru>
2025-08-13 12:31:03 +0300

    iter22

--- internal/config/agentConfig.go
+++ internal/config/agentConfig.go
@@ -1,10 +1,13 @@
 package config
 
 import (
+	"encoding/json"
 	"flag"
-	"github.com/caarlos0/env/v6"
+	"os"
 	"reflect"
 	"time"
+
+	"github.com/caarlos0/env/v6"
 )
 
 type AgentConfig struct {
@@ -14,23 +17,71 @@ type AgentConfig struct {
 	CryptoKey     string        `env:"CRYPTO_KEY"`
 }
 
-func NewAgentConfig() *AgentConfig {
-	var cfg AgentConfig
-	var repInt int
-	var pollInt int
-	var addr string
-	var cryptoKey string
+type agentFileConfig struct {
+	Address        *string `json:"address"`
+	ReportInterval *string `json:"report_interval"`
+	PollInterval   *string `json:"poll_interval"`
+	CryptoKey      *string `json:"crypto_key"`
+}
 
-	flag.StringVar(&addr, "a", "localhost:8080", "address and port to run server")
-	flag.IntVar(&repInt, "r", 10, "report send interval")
-	flag.IntVar(&pollInt, "p", 2, "poll interval")
-	flag.StringVar(&cryptoKey, "crypto-key", "", "path to RSA public key (PEM)")
+func NewAgentConfig() *AgentConfig {
+	// Defaults
+	cfg := AgentConfig{
+		AddressServer: "localhost:8080",
+		SendInterval:  10 * time.Second,
+		PollInterval:  2 * time.Second,
+		CryptoKey:     "",
+	}
+
+	configEnvPath := os.Getenv("CONFIG")
+
+	var (
+		addrFlag      string
+		repIntFlag    int
+		pollIntFlag   int
+		cryptoFlag    string
+		configFlag    string
+		configFlagAlt string
+	)
+	flag.StringVar(&addrFlag, "a", cfg.AddressServer, "address and port to run server")
+	flag.IntVar(&repIntFlag, "r", int(cfg.SendInterval/time.Second), "report send interval")
+	flag.IntVar(&pollIntFlag, "p", int(cfg.PollInterval/time.Second), "poll interval")
+	flag.StringVar(&cryptoFlag, "crypto-key", cfg.CryptoKey, "path to RSA public key (PEM)")
+	flag.StringVar(&configFlag, "config", "", "path to JSON config file")
+	flag.StringVar(&configFlagAlt, "c", "", "path to JSON config file (short)")
 	flag.Parse()
 
-	cfg.AddressServer = addr
-	cfg.SendInterval = time.Duration(repInt) * time.Second
-	cfg.PollInterval = time.Duration(pollInt) * time.Second
-	cfg.CryptoKey = cryptoKey
+	configPath := configFlag
+	if configPath == "" {
+		configPath = configFlagAlt
+	}
+	if configPath == "" {
+		configPath = configEnvPath
+	}
+
+	if configPath != "" {
+		if data, err := os.ReadFile(configPath); err == nil {
+			var fc agentFileConfig
+			if err := json.Unmarshal(data, &fc); err == nil {
+				if fc.Address != nil {
+					cfg.AddressServer = *fc.Address
+				}
+				if fc.ReportInterval != nil {
+					if d, err := time.ParseDuration(*fc.ReportInterval); err == nil {
+						cfg.SendInterval = d
+					}
+				}
+				if fc.PollInterval != nil {
+					if d, err := time.ParseDuration(*fc.PollInterval); err == nil {
+						cfg.PollInterval = d
+					}
+				}
+				if fc.CryptoKey != nil {
+					cfg.CryptoKey = *fc.CryptoKey
+				}
+			}
+		}
+	}
 
 	err := env.ParseWithFuncs(&cfg, map[reflect.Type]env.ParserFunc{
 		reflect.TypeOf(time.Duration(0)): func(value string) (interface{}, error) {
@@ -48,6 +99,23 @@ func NewAgentConfig() *AgentConfig {
 	)
 	if err != nil {
 		return nil
+	}
+
+	setFlags := map[string]bool{}
+	flag.Visit(func(f *flag.Flag) {
+		setFlags[f.Name] = true
+	})
+	if setFlags["a"] {
+		cfg.AddressServer = addrFlag
+	}
+	if setFlags["r"] {
+		cfg.SendInterval = time.Duration(repIntFlag) * time.Second
+	}
+	if setFlags["p"] {
+		cfg.PollInterval = time.Duration(pollIntFlag) * time.Second
+	}
+	if setFlags["crypto-key"] {
+		cfg.CryptoKey = cryptoFlag
 	}
 
 	return &cfg
